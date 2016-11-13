da43fadd6d4fbf611ea7398bbeb4c05fa9576601
Roshin Mikhail <roshin.mikhail2016@yandex.ru>
2016-11-13 19:20:55 +0300

    first commit

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication9.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello world!\n";
+	system("pause");
+    return 0;
+}
+
